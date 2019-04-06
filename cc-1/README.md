f3a000e2db0e2f09ba1c5589b25e69b87c26ecb9
SeanErvinson <ervinsonong@gmail.com>
2019-04-06 16:18:34 +0800

    cc-1

---
+++ cc-1/README.md
@@ -0,0 +1,15 @@
+# Coding Challenge #1
+
+### Language
+Today's language is C#
+
+## Instruction
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+## Example
+Given [10, 15, 3, 7] and k of 17,
+
+return **true** since 10 + 7 is 17.
+
+## Contributing
+Feel free to contribute to this project.
---
+++ cc-1/src/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 15, 21, 5, 6, 3, 7 };
+            int target = 13;
+            System.Console.WriteLine(IsSum(numbers, target));
+        }
+
+        private static bool IsSum(int[] numbers, int target)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (var number in numbers)
+            {
+                var missing = Math.Abs(target - number);
+                if (set.Contains(number))
+                    return true;
+                set.Add(missing);
+            }
+            return false;
+        }
+    }
+}
